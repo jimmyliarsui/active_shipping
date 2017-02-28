50d6a7c78c5394717c85e7db2e9f7c7c494b9611
Yukang Chen <yukang.chen@dji.com>
2017-02-28 18:01:26 +0800

    fix sf live

--- lib/active_shipping/carriers/sf.rb
+++ lib/active_shipping/carriers/sf.rb
@@ -8,7 +8,7 @@ module ActiveShipping
     @@name = "SF"
 
     TEST_URL = "http://218.17.248.244:11080/bsp-oisp/sfexpressService"
-    LIVE_URL = "not implemented"
+    LIVE_URL = "http://bsp-ois.sf-express.com/bsp-ois/sfexpressService"
 
     def requirements
       [:monthly_account, :checkword]
